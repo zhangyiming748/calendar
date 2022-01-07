61921377fbcb44df04ce14275287872d25964306
v-zhangyiming1 <v-zhangyiming1@360.cn>
2022-01-07 13:13:31 +0800

    添加上传文本文件接口

--- Calendar.go
+++ Calendar.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/zhangyiming748/calendar/v3/controller"
+	"log"
+	"net/http"
 	"strconv"
 	"strings"
 )
@@ -22,6 +24,36 @@ func ShowWeb(port int) {
 	r.GET("/HappyCount", controller.HappyDayCountdown)
 	r.GET("/SadCount", controller.SadDayCountdown)
 	r.GET("/", controller.StandRequest)
+	/*
+		r.POST("/upload", func(c *gin.Context) {
+			file, err := c.FormFile("file")
+			if err != nil {
+				c.String(500, "上传图片出错")
+			}
+			// c.JSON(200, gin.H{"message": file.Header.Context})
+			c.SaveUploadedFile(file, file.Filename)
+			c.String(http.StatusOK, file.Filename)
+		})
+	*/
+	r.POST("/upload", func(c *gin.Context) {
+		_, headers, err := c.Request.FormFile("file")
+		if err != nil {
+			log.Printf("Error when try to get file: %v", err)
+		}
+		//headers.Size 获取文件大小
+		if headers.Size > 1024*1024*2 {
+			fmt.Println("文件太大了")
+			return
+		}
+		//headers.Header.Get("Content-Type")获取上传文件的类型
+		if headers.Header.Get("Content-Type") != "text/plain" {
+			fmt.Println("只允许上传txt文本文件")
+			return
+		}
+		//c.SaveUploadedFile(headers, "./video/"+headers.Filename)
+		c.SaveUploadedFile(headers, "./users.txt")
+		c.String(http.StatusOK, headers.Filename)
+	})
 	//3.监听端口，默认在8080
 	//Run("里面不指定端口号默认为8080")
 	if err := r.SetTrustedProxies([]string{"0.0.0.0"}); err != nil {
--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 打工人的日历
  - [x] 初步实现通过网络访问
 # API
+
 #### {GET}
+
 - [x] /
 - [x] /HappyCount
 - [x] /SadCount
+
+#### {POST}
+
+- [x] /upload //(覆盖)上传文本文件名单
